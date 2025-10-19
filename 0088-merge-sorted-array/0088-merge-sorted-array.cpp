35976a859672e5fd6be8f731ce239bb5b1a94028
sumitpandey48 <pandeysumit28130@gmail.com>
2025-10-20 00:24:53 +0530

    Time: 0 ms (100%), Space: 12.3 MB (70.82%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,30 +1,46 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int nums3[n + m];
-        int first = 0;
-        int second = 0;
-        int index = 0;
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
 
-        while (first < m && second < n) {
-            if (nums1[first] <= nums2[second]) {
-                nums3[index] = nums1[first];
-                first++, index++;
-            } else {
-                nums3[index] = nums2[second];
-                second++, index++;
+        while (gap > 0) {
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+
+                // Case 1: both pointers in nums1
+                if (left < m && right < m) {
+                    if (nums1[left] > nums1[right]) {
+                        swap(nums1[left], nums1[right]);
+                    }
+                }
+                // Case 2: left in nums1, right in nums2
+                else if (left < m && right >= m) {
+                    if (nums1[left] > nums2[right - m]) {
+                        swap(nums1[left], nums2[right - m]);
+                    }
+                }
+                // Case 3: both in nums2
+                else {
+                    if (nums2[left - m] > nums2[right - m]) {
+                        swap(nums2[left - m], nums2[right - m]);
+                    }
+                }
+
+                left++;
+                right++;
             }
+
+            if (gap == 1)
+                break;
+            gap = (gap / 2) + (gap % 2);
         }
 
-        while (first < m) {
-            nums3[index++] = nums1[first++];
-        }
-        while (second < n) {
-            nums3[index++] = nums2[second++];
-        }
-
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = nums3[i];
+        // Copy nums2 values to nums1 at the end
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
         }
     }
 };
