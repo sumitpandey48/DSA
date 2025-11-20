79e86173cd6c38e263329339b7eb88f1ffca9d8e
sumitpandey48 <pandeysumit28130@gmail.com>
2025-11-21 01:02:30 +0530

    Time: 7 ms (74.04%), Space: 23 MB (45.05%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -2,23 +2,30 @@ class Solution {
 public:
     int findmax(vector<int>& piles) {
         int maxi = INT_MIN;
-        for (int i = 0; i < piles.size(); i++) {
+        int n = piles.size();
+        for (int i = 0; i < n; i++) {
             maxi = max(maxi, piles[i]);
         }
         return maxi;
     }
+
     long long findHours(vector<int>& piles, int hourly) {
         long long totalH = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            totalH += ceil((double)piles[i] / (double)hourly);
+        int n = piles.size();
+        for (int i = 0; i < n; i++) {
+            totalH += (piles[i] + hourly - 1) / hourly;
         }
         return totalH;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
         int left = 1, right = findmax(piles);
+
         while (left <= right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
+
             long long totalH = findHours(piles, mid);
+
             if (totalH <= h) {
                 right = mid - 1;
             } else {
