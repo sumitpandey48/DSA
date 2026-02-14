ba0da0a502d75cfdebaeea6a52b52884f0d1c961
sumitpandey48 <pandeysumit28130@gmail.com>
2026-02-14 17:47:40 +0530

    Time: 51 ms (24.28%), Space: 75.8 MB (17.94%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,23 +10,53 @@
  */
 class Solution {
 public:
+    ListNode* mergeSortedLL(ListNode* list1, ListNode* list2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+
+        while (list1 != nullptr && list2 != nullptr) {
+            if (list1->val <= list2->val) {
+                temp->next = list1;
+                list1 = list1->next;
+            } else {
+                temp->next = list2;
+                list2 = list2->next;
+            }
+            temp = temp->next;
+        }
+        if (list1 != nullptr) {
+            temp->next = list1;
+        } else {
+            temp->next = list2;
+        }
+        return dummyNode->next;
+    }
+    ListNode* findMiddle(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head->next->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
     ListNode* sortList(ListNode* head) {
         if (head == nullptr || head->next == nullptr) {
             return head;
         }
-        vector<int> arr;
-        ListNode* temp = head;
+        ListNode* middle = findMiddle(head);
 
-        while (temp != nullptr) {
-            arr.push_back(temp->val);
-            temp = temp->next;
-        }
-        sort(arr.begin(), arr.end());
-        temp = head;
-        for (int i = 0; i < arr.size(); i++) {
-            temp->val = arr[i];
-            temp = temp->next;
-        }
-        return head;
+        ListNode* right = middle->next;
+        middle->next = nullptr;
+        ListNode* left = head;
+
+        left = sortList(left);
+        right = sortList(right);
+
+        return mergeSortedLL(left, right);
     }
 };
