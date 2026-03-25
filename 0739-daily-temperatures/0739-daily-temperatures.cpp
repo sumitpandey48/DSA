3bc295063d5e7c4935036c72e0416843fdaf7551
sumitpandey48 <pandeysumit28130@gmail.com>
2026-03-25 23:51:35 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> dailyTemperatures(vector<int>& temp) {
         int n = temp.size();
         vector<int>ans(n,0);
-        stack<pair<int,int>>s;
+        stack<int>s;
 
         for(int i = n-1;i>=0;i--){
-            while(!s.empty() && s.top().first<= temp[i]) s.pop();
+            while(!s.empty() && temp[s.top()]<= temp[i]) s.pop();
             
-            if(!s.empty()) ans[i] = s.top().second - i;
+            if(!s.empty()) ans[i] = s.top() - i;
 
-            s.push({temp[i],i});
+            s.push(i);
         }
         return ans;
     }
