023b3af4008d08a5fcf7b47853a5640f01f825ee
sumitpandey48 <pandeysumit28130@gmail.com>
2026-04-27 14:54:31 +0530

    Time: 4 ms (33.76%), Space: 15.4 MB (40.34%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void solve(string ip, string op, vector<string>& ans) {
+        if (ip.length() == 0) {
+            ans.push_back(op);
+            return;
+        }
+        string op1 = op;
+        string op2 = op;
+
+        if (isalpha(ip[0])) {
+            op1.push_back(tolower(ip[0]));
+            op2.push_back(toupper(ip[0]));
+
+            ip.erase(ip.begin() + 0);
+            solve(ip, op1, ans);
+            solve(ip, op2, ans);
+        } else {
+            op.push_back(ip[0]);
+            ip.erase(ip.begin() + 0);
+            solve(ip, op, ans);
+            return;
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        vector<string> ans;
+        string op = "";
+        solve(s, op, ans);
+        return ans;
+    }
+};
