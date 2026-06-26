30af90e2b9485596dbb902ebc55b7e80ac1115d4
sumitpandey48 <pandeysumit28130@gmail.com>
2026-06-26 23:06:13 +0530

    Time: 0 ms (100%), Space: 27.3 MB (52.08%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,7 +4,9 @@ public:
         int i = 0, j = s.size() - 1;
 
         while (i < j) {
-            swap(s[i], s[j]);
+            int a = s[i];
+            s[i] = s[j];
+            s[j] = a;
             i++;
             j--;
         }
