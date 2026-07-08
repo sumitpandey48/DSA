cc71ce03bd0bfd2c25b40bfb33682ba3700c9719
sumitpandey48 <pandeysumit28130@gmail.com>
2026-07-09 01:47:00 +0530

    Time: 16 ms (60.01%), Space: 26 MB (49.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n = strs.size();
         vector<vector<string>> ans;
-        unordered_map<string, vector<string>> mp;
+        unordered_map<string, vector<string>> mpp;
         for (int i = 0; i < n; i++) {
             string a = strs[i];
             sort(begin(a), end(a));
-            mp[a].push_back(strs[i]);
+            mpp[a].push_back(strs[i]);
         }
-        for (auto it : mp) {
+        for (auto it : mpp) {
             ans.push_back(it.second);
         }
         return ans;
